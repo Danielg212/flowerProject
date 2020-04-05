d4a5ee160ab5c8f6e4b3913dd18b9cfcb949574b
Daniel Giat <dgiat@outbrain.com>
2020-04-05 15:03:43 +0300

    security

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCd7uw_PfjxweKZKTUcG2NZ7WraK0stbkE',
-    authDomain: 'flower-add2b.firebaseapp.com',
-    databaseURL: 'https://flower-add2b.firebaseio.com',
-    projectId: 'flower-add2b',
-    storageBucket: 'flower-add2b.appspot.com',
-    messagingSenderId: '222084360243',
-    appId: '1:222084360243:web:44751a0dea2eeb1cf19b46',
-    measurementId: 'G-EQB3NG8777'
+
   }
 };
 
